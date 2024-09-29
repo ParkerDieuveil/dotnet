a5b2971913c5403ed077cc7e71ab94c544ce5eaf
unknown <dieuveilparkermahoukou@gmail.com>
2024-09-29 14:09:09 +0200

    atelier1

---
+++ Atelier1/Controllers/EmployeeController.cs
@@ -0,0 +1,116 @@
+﻿using Atelier1.Models.Repositories;
+using Atelier1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atelier1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        readonly IRepository<Employee> employeeRepository;
+
+        //injection de dépendance
+        public EmployeeController(IRepository<Employee> empRepository)
+        {
+
+            employeeRepository = empRepository;
+
+        }
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            var employees = employeeRepository.GetAll();
+            ViewData["EmployeesCount"] = employees.Count();
+            ViewData["SalaryAverage"] = employeeRepository.SalaryAverage();
+            ViewData["MaxSalary"] = employeeRepository.MaxSalary();
+            ViewData["HREmployeesCount"] = employeeRepository.HrEmployeesCount();
+            return View(employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+
+            return View(employee);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+           
+            return View();
+            
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employeeRepository.Add(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Employee newEmployee)
+        {
+            if (id != newEmployee.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                employeeRepository.Update(id, newEmployee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(newEmployee);
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            employeeRepository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public ActionResult Search(string term)
+        {
+
+            var result = employeeRepository.Search(term);
+            return View("Index", result);
+        }
+        
+    }
+}
